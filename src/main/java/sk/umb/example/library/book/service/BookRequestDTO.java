a6f2f6cad68ed5b049e41d55937ee76ae06565fa
Kuba <mkuba@student.umb.sk>
2023-02-26 22:31:11 +0100

    Fixed porting issue

---
+++ src/main/java/sk/umb/example/library/book/service/BookRequestDTO.java
@@ -0,0 +1,52 @@
+package sk.umb.example.library.book.service;
+
+public class BookRequestDTO {
+    private String authorFirstName;
+    private String authorLastName;
+
+    private String title;
+
+    private int isbn;
+
+    private int count;
+
+    public String getAuthorFirstName() {
+        return authorFirstName;
+    }
+
+    public void setAuthorFirstName(String AuthorFirstName) {
+        this.authorFirstName = AuthorFirstName;
+    }
+
+    public String getAuthorLastName() {
+        return authorLastName;
+    }
+
+    public void setAuthorLastName(String AuthorLastName) {
+        this.authorLastName = AuthorLastName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String Title) {
+        this.title = Title;
+    }
+
+    public void setIsbn(int ISBN) {
+        this.isbn = ISBN;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setCount(int num) {
+        this.count += num;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
